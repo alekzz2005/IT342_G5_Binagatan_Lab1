deab59a5e32f17b8b0596dd0d638a658405d73a1
alekzz2005 <binagatanalexander2005@gmail.com>
2026-02-06 09:31:35 +0800

    Initialize Spring Boot backend

---
+++ backend/src/main/java/com/it342/auth/UserAuthenticationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.it342.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthenticationBackendApplication.class, args);
+	}
+
+}
